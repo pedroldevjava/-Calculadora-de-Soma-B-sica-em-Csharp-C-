c59f4e5c47b070585aa52c2bd0767ad83243140b
pedroldevjava <pedroldevjava@gmail.com>
2025-11-19 07:36:48 -0300

    ~ Calculadora de Soma Básica

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace matematicaSOMA {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite dois números:");
+
+            int num1 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            int num2 = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int soma = num1 + num2;
+
+
+            Console.WriteLine("Soma = " + soma);
+
+        }
+    }
+}
